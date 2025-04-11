dd96bfc5c246681037853e7aef5cdbc038309131
mikaze <146914469+mikaze0067@users.noreply.github.com>
2025-04-11 12:06:51 +0900

    02-02完了

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -8,14 +8,19 @@ void Particle::Initialize(Model* model) {
 	model_ = model;
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
+	//色の設定
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
 }
 
 void Particle::Update() {
 	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+	//色変更オブジェクトに色の数値を設定する
+	objectColor_.SetColor(color_);
 }
 
 void Particle::Draw(Camera& camera) {
 	//3Dモデルを描画
-	model_->Draw(worldTransform_, camera);
+	model_->Draw(worldTransform_, camera,&objectColor_);
 }
