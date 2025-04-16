a01abb9276e01ed83ade888d7e8d50425bd36cae
mikaze <146914469+mikaze0067@users.noreply.github.com>
2025-04-16 13:04:08 +0900

    PSOまで

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,11 +1,15 @@
 #include "GameScene.h"
 #include "KamataEngine.h"
 #include <Windows.h>
+#include <d3dcompiler.h>
 
 using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_07_ササノ_ミカゼ_CG4");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
@@ -15,8 +19,123 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// ゲームシーンの初期化
 	gameScene->Initialize();
 
-	// エンジンの初期化
-	KamataEngine::Initialize(L"LE3D_07_ササノ_ミカゼ_CG4");
+#pragma region RootSignature作成
+	//構造体にデータを用意する
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags =
+	D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlog = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+	D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(
+			reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+		assert(false);
+	}
+	//バイナリをもとに生成
+	ID3D12RootSignature* rootSignature = nullptr;
+	hr = dxCommon->GetDevice()->CreateRootSignature(
+		0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(),
+		IID_PPV_ARGS(&rootSignature));
+	assert(SUCCEEDED(hr));
+
+	//InputLayout
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+#pragma endregion
+
+#pragma region BlendState
+	D3D12_BLEND_DESC blendDesc{};
+	//すべての色素を書き込む
+	blendDesc.RenderTarget[0].RenderTargetWriteMask =
+	D3D12_COLOR_WRITE_ENABLE_ALL;
+#pragma endregion
+
+#pragma region RasterizeState
+	D3D12_RASTERIZER_DESC rasterizeDesc{};
+	//裏面（反時計回り）をカリングする
+	rasterizeDesc.CullMode = D3D12_CULL_MODE_BACK;
+	//塗りつぶしモードをソリッドにする（ワイヤーフレームならD3D12_FILL_MODE_WIREFRAME）
+	rasterizeDesc.FillMode = D3D12_FILL_MODE_SOLID;
+#pragma endregion
+
+#pragma region VertexShaderをCompile
+	//コンパイル済みのShader,エラー時情報の格納場所の用意
+	ID3DBlob* vsBlob = nullptr;
+	ID3DBlob* psBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+	//頂点シェーダーの読み込みとコンパイル
+	std::wstring vsFile = L"Resources/sahders/TestVS.hlsl";
+	hr = D3DCompileFromFile(vsFile.c_str(),
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"main", "vs_5_0",
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+		0, &vsBlob, &errorBlob);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(
+			std::system_category().message(hr).c_str());
+		if (errorBlob) {
+			DebugText::GetInstance()->ConsolePrintf(
+				reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+#pragma endregion
+
+#pragma region PixelShaderをCompile
+	//PixelShaderの読み込みとコンパイル
+	std::wstring psFile = L"Resource/Shaders/TestPS.hlsl";
+	hr = D3DCompileFromFile(psFile.c_str(),
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"main", "ps_5_0",
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+		0, &psBlob, &errorBlob);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(
+			std::system_category().message(hr).c_str());
+		if (errorBlob) {
+			DebugText::GetInstance()->ConsolePrintf(
+				reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+#pragma endregion
+
+#pragma region PSO(PiplineStateObject)の生成
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
+	graphicsPipelineStateDesc.pRootSignature = rootSignature;
+	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc;
+	graphicsPipelineStateDesc.VS = {vsBlob->GetBufferPointer(), vsBlob->GetBufferSize()};
+	graphicsPipelineStateDesc.PS = {psBlob->GetBufferPointer(), psBlob->GetBufferSize()};
+	graphicsPipelineStateDesc.BlendState = blendDesc;
+	graphicsPipelineStateDesc.RasterizerState = rasterizeDesc;
+	//書き込むRTVの情報
+	graphicsPipelineStateDesc.NumRenderTargets = 1;
+	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	//利用するトポロジ（形状）のタイプ。三角形
+	graphicsPipelineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	//どのように画面に色を打ち込むのか設定
+	graphicsPipelineStateDesc.SampleDesc.Count = 1;
+	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	//PSOの生成
+	ID3D12PipelineState* graphicsPipelineState = nullptr;
+	hr = dxCommon->GetDevice()->CreateGraphicsPipelineState(
+		&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
+	assert(SUCCEEDED(hr));
+
+#pragma endregion
+
 
 	// メインループ
 	while (true) {
