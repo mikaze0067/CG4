4588af04f75715adcccf25dd6d51b24111678f9a
mikaze <146914469+mikaze0067@users.noreply.github.com>
2025-06-09 22:08:26 +0900

    d

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -12,9 +12,21 @@ void GameScene::Initialize() {
 
 	Model2::StaticInitialize();
 
+	model2_ = Model2::Create();
 }
 
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+
+// DirectXCommon インスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+	// パーティクル描画
+	model2_->Draw();
+
+	// 3Dモデル描画後処理
+	Model::PostDraw();
+}
 
