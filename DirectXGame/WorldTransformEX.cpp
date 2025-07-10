9ee75b673fc4f86616ae6b7bb2ed89fe1fa00e80
mikaze <146914469+mikaze0067@users.noreply.github.com>
2025-07-10 16:46:05 +0900

    10

--- DirectXGame/WorldTransformEX.cpp
+++ DirectXGame/WorldTransformEX.cpp
@@ -1,5 +1,28 @@
 #include "WorldTransformEX.h"
 
-void WorldTransformEX::UpdateMatrix() {}
+using namespace KamataEngine;
+using namespace KamataEngine::MathUtility;
 
-KamataEngine::Matrix4x4 WorldTransformEX::MakeAffineMatrix() { return KamataEngine::Matrix4x4(); }
+void WorldTransformEX::UpdateMatrix() {
+	// World変換行列を計算し、matWorld_に格納する
+	matWorld_ = MakeAffineMatrix();
+	//定数バッファへ転送する
+	TransferMatrix();
+}
+
+Matrix4x4 WorldTransformEX::MakeAffineMatrix() {
+	//Scale Matrix
+	Matrix4x4 matScale = MakeScaleMatrix(scale_);
+	
+	Matrix4x4 matRotX = MakeRotateXMatrix(rotation_.x);
+	Matrix4x4 matRotY = MakeRotateXMatrix(rotation_.y);
+	Matrix4x4 matRotZ = MakeRotateXMatrix(rotation_.z);
+	Matrix4x4 matRot = matRotZ * matRotX * matRotY;
+
+	// TranslateMatrix
+	Matrix4x4 matTrans = MakeTranslateMatrix(translation_);
+	//worldMatrix
+	Matrix4x4 matWorld = matScale * matRot * matTrans;
+
+	return matWorld;
+}
